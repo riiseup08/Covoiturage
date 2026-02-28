f2523aef3bc511c6bc69e97028d7d5a1e94c9617
Eric <monthe1994@gmail.com>
2026-02-27 23:58:48 -0500

    Fix ALLOWED_HOSTS for Render

--- carpoolconfig/settings.py
+++ carpoolconfig/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = os.environ.get(
 )
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = os.environ.get('DJANGO_DEBUG', 'True').lower() in ('true', '1', 'yes')
+DEBUG = False
 
 ALLOWED_HOSTS = ['covoiturage-8gqc.onrender.com', 'localhost', '127.0.0.1']
 
